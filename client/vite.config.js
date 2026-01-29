49ae98b4ce52db18bf3950cd5382402de5fdd6f1
Web-Dev-With-Dev <gondaliyadev71@gmail.com>
2026-01-29 23:31:50 +0530

    debugged

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': import.meta.env.VITE_API_URL
     }
   }
 })
